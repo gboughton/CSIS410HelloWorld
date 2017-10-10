91735fd252ce33c78bf4e6d59c446156df367af0
Tristan Canova <tcanova1@gmail.com>
2017-10-10 18:35:49 -0400

    "Added some very important info"

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 /**
  * Write a description of class HelloWorld here.
  *
- * @author (your name)
+ * @author Tristan Canova and Jessie Leary 10/10/17
  * @version (a version number or a date)
  */
 public class HelloWorld
@@ -10,5 +10,6 @@ public class HelloWorld
     public static void main()
     {
         System.out.println("Hello World");
+        
     }
 }
